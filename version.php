ca4661bade5c7a87823791661e0b6b2b9b79190f
root <pauloafpjunior@hotmail.com>
2019-02-15 16:40:18 -0200

    update version

--- version.php
+++ version.php
@@ -11,5 +11,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2019021415; // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015111605; // Requires this Moodle version
 $plugin->component = 'tool_brcli'; // Full name of the plugin (used for diagnostics)
-$plugin->maturity = 'stable';
+$plugin->maturity = MATURITY_BETA;
 $plugin->release = 'v1.0';
